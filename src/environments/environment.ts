cf4dfab9c1df50bef472a352443e7b4f7524194d
Royal6969 <sergiodiazcampos@gmail.com>
2021-11-29 17:20:26 +0100

    uploading the whole practice - 1

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+
+  firebaseConfig: {
+    apiKey: "AIzaSyD_pV3JLTk8AthsGB19uXDdvQxC2DHeFWM",
+    authDomain: "empleados-crud-firestore.firebaseapp.com",
+    projectId: "empleados-crud-firestore",
+    storageBucket: "empleados-crud-firestore.appspot.com",
+    messagingSenderId: "17949636770",
+    appId: "1:17949636770:web:2050dc500321ef6e9b7ffb"
+  }
+};
